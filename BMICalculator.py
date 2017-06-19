959d874b433c3b627bfc72357ed2032b0e98d589
RodriguezGarcia_Manuel <rodrm787@hsd.k12.or.us>
2017-06-19 11:27:11 -0700

    adding successful code

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,10 +1,13 @@
 # BMICalculator.py
 # Your job is to write a BMI calculator that matches the formula
 # and chart on http://flightphysical.com/medical-exam/weight
-
+import math
 # Define Function below
 # be sure to return an integer
-
+def calculateBMI(weight, height):
+    bmi = (weight*703)/(height*height)
+    bmi = round(bmi, 2)
+    return bmi
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
